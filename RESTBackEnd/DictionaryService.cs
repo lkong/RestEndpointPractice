8bccf8289e7c206f3f475fd953bae668c2cbc2c7
lkong <lkong@pertius.com>
2015-10-29 15:18:15 -0500

    setup the structure

---
+++ RESTBackEnd/DictionaryService.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTBackEnd
+{
+    public class DictionaryService : IService
+    {
+    }
+}
