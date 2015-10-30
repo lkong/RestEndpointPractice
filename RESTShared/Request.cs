e9901d35c8b13ed34e4f638bd4b2ae3c72936802
lkong <lkong@pertius.com>
2015-10-29 20:28:34 -0500

    add files

---
+++ RESTShared/Request.cs
@@ -0,0 +1,16 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTShared
+{
+    [Route("/spellcheckrequest") ]
+    [Route("/spellcheckrequest/{Word}") ]
+    public class SpellCheckRequest
+    {
+        public string Word;
+    }
+}
---
+++ RESTShared/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTShared
+{
+    public class Response
+    {
+        public string Word;
+        public int Rank;
+    }
+}
