6a2516e88f191152e5e8d8d5f432e79535db3dbb
lkong <lkong@pertius.com>
2015-10-29 20:20:29 -0500

    rest service setup

---
+++ RESTService/Global.asax.cs
@@ -0,0 +1,80 @@
+﻿using Funq;
+using RESTShared;
+using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace RESTService
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            (new SpellCheckAppHost()).Init();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class DictionaryService : IService
+    {
+        public object Any(SpellCheckRequest request)
+        {
+            return request.Word;
+        }
+    }
+    public class SpellCheckAppHost : AppHostBase
+        {
+            /// <summary>
+            /// Initializes a new instance of your ServiceStack application, with the specified name and assembly containing the services.
+            /// </summary>
+            public SpellCheckAppHost() : base("Spellcheck Web Services", typeof(DictionaryService).Assembly) { }
+ 
+            /// <summary>
+            /// Configure the container with the necessary routes for your ServiceStack application.
+            /// </summary>
+            /// <param name="container">The built-in IoC used with ServiceStack.</param>
+            public override void Configure(Container container)
+            {
+                //Register user-defined REST-ful urls. You can access the service at the url similar to the following.
+                //http://localhost/ServiceStack.Hello/servicestack/hello or http://localhost/ServiceStack.Hello/servicestack/hello/John%20Doe
+                //You can change /servicestack/ to a custom path in the web.config.
+                Routes
+                  .Add<SpellCheckRequest>("/spellcheckrequest")
+                  .Add<SpellCheckRequest>("/spellcheckrequest/{Word}");
+            }
+        }
+
+}
